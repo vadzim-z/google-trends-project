8ba96ed314b02231d771315ac30513786c0371c2
vadzim-z <vadzim.zylevich@maersk.com>
2023-02-08 15:57:42 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vadzim-z/google-trends-project
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel *Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Title    string `xml:"title"`
+	ItemList []Item `xml:"item"`
+}
+
+type Item struct {
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"approx_traffic"`
+	NewsItems []News `xml:"news_item"`
+}
+
+type News struct {
+	Headline     string `xml:"news_item_title"`
+	HeadlineLink string `xml:"news_item_url"`
+}
+
+func main() {
+	var r RSS
+
+	data := readGoogleTrends()
+
+	err := xml.Unmarshal(data, &r)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println("\n Below are all the Google Search Trends For Today!")
+	fmt.Println("---------------------------------------------------")
+
+	for i := range r.Channel.ItemList {
+		rank := (i + 1)
+		fmt.Println("#", rank)
+		fmt.Println("Search term:", r.Channel.ItemList[i].Title)
+		fmt.Println("Link to the Trend:", r.Channel.ItemList[i].Link)
+
+		for j := range r.Channel.ItemList[i].NewsItems {
+			fmt.Println("Headline:", r.Channel.ItemList[i].NewsItems[j].Headline)
+			fmt.Println("Link to the Article:", r.Channel.ItemList[i].NewsItems[j].HeadlineLink)
+		}
+		fmt.Println("------------------------------------------------")
+	}
+}
+
+func readGoogleTrends() []byte {
+	resp := getGoogleTrends()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return data
+}
+
+func getGoogleTrends() *http.Response {
+	resp, err := http.Get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return resp
+}
